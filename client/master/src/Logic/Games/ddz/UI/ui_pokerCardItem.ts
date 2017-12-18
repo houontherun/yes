2e52827502f0bfa5c376560bc14c864e7fb4e792
yanwei2016 <yanwei47@163.com>
2017-12-18 13:55:33 +0800

    扑克牌UI和逻辑更新

--- client/master/src/Logic/Games/ddz/UI/ui_pokerCardItem.ts
+++ client/master/src/Logic/Games/ddz/UI/ui_pokerCardItem.ts
@@ -112,7 +112,15 @@ export class ui_pokerCardItem extends gameUI.UIbase implements  eui.UIComponent 
 		{
 			this.img_index0.visible  = false;
 			this.img_suit0.visible = false;
+
+		 if(this.isJoker)
+		  {
+			  this.img_index.scaleX = 0.55;
+			  this.img_index.scaleY = 0.55;
+		  }
 		}
+
+		
 	}
 
     public get Selected():boolean{
