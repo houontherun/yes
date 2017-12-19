a6daeaecc39c491beabb368ceb0f6325f1ec778a
yanwei2016 <yanwei47@163.com>
2017-12-19 20:37:02 +0800

    添加一些错误判断

--- client/master/src/Logic/Games/ddz/UI/ddz_game.ts
+++ client/master/src/Logic/Games/ddz/UI/ddz_game.ts
@@ -369,12 +369,12 @@ namespace gameUI {
             var i: number = 0;
             var group = this.GetGroupChairid(chairid);
             let Scorepos = group.getChildByName("Label_pos");
-            let startposX: number = 0;
+            let startposX: number = -10;
             let posY = 60;
             
             startposX = Scorepos.x;
             if (startposX < 10) {
-                startposX = startposX - 34 * cards.length;
+                startposX = startposX - 34 * Totalnum;
             }
             
             let addBrightcardTimer = CardLogic.Timer.Instance.Repeat(0.18, () => {
