970677fa859c37e7a190a4f109f26720281205de
yanwei2016 <yanwei47@163.com>
2017-12-20 10:06:23 +0800

    后台运行

--- client/master/src/Main.ts
+++ client/master/src/Main.ts
@@ -48,11 +48,11 @@ class Main extends eui.UILayer {
         })
 
         egret.lifecycle.onPause = () => {
-            egret.ticker.pause();
+           // egret.ticker.pause();
         }
 
         egret.lifecycle.onResume = () => {
-            egret.ticker.resume();
+           // egret.ticker.resume();
         }
 
         //注入自定义的素材解析器
